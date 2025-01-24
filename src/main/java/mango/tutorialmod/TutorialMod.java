3f28d6f9be0d8343aeefc9fd2198f7177b741429
TheMangoSpider <matheus.werneck2006@gmail.com>
2025-01-23 20:00:05 -0800

    jit

--- src/main/java/mango/tutorialmod/TutorialMod.java
+++ src/main/java/mango/tutorialmod/TutorialMod.java
@@ -1,16 +1,17 @@
 package mango.tutorialmod;
 
+import mango.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID = "tutorial-mod";
+	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/mango/tutorialmod/item/ModItems.java
@@ -0,0 +1,26 @@
+package mango.tutorialmod.item;
+
+import mango.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item SPECIAL_JIT = registerItem("special_jit", new Item(new Item.Settings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(SPECIAL_JIT);
+        });
+    }
+}
