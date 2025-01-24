35c9bd5b68b2ee6a5ae4e274c4045369ca5d80f3
TheMangoSpider <matheus.werneck2006@gmail.com>
2025-01-23 19:02:11 -0800

    Initial commit

---
+++ src/main/java/mango/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package mango.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
